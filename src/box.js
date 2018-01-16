813649cd1e82d9eccaff02f107f5375ad0335b74
Kamal Kokne <kamalkokne4@gmail.com>
2018-01-16 18:07:33 +0530

    Created box.js
    This is to demo how inline CSS is applied in React Component.

---
+++ src/box.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const divStyle = {
+    margin: '40px',
+    border: '5px solid pink'
+};
+
+const pStyle = {
+    fontSize: '15px',
+    textAlign: 'center'
+};
+
+const Box = () => (
+    <div style={divStyle}>
+        <p style={pStyle}>Inline Styling</p>
+    </div>
+);
+
+export default Box;
