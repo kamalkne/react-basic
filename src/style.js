c4896eed2e8c93f150e7d1e3c706c77e88a9c00d
Kamal Kokne <kamalkokne4@gmail.com>
2018-01-16 18:05:03 +0530

    Created Style.js
    This is demo that how CSS classes are applied in react Component

---
+++ src/style.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class style extends Component {
+    render() {
+        return(
+            <div className="DottedBox">
+                <p className="DottedBox_content">CSS Style</p>
+            </div>
+        )
+    }
+}
+
+export default style;
