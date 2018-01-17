0361dcc6ceee019c306f2be9d184591fe3d88aad
Kamal Kokne <kamalkokne4@gmail.com>
2018-01-17 16:47:29 +0530

    Added module.js

---
+++ src/module/module.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './mudule.css';
+
+const DashedB = () => (
+    <div className={styles.container}>
+        <p className={styles.content}>This is Module CSS</p>
+    </div>
+)
+
+export default DashedB;
